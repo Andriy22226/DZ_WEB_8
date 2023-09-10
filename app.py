9548eaa42d25bf60fc4530126c40b4282554429e
Andriy <127453425+Andriy22226@users.noreply.github.com>
2023-09-10 11:33:26 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from pprint import pprint
+
+
+from connect import cache
+from models import Autor, Post, Tag
+
+@cache
+def find_tag(tag_name):
+    posts = Post.objects(tags__name__contains=tag_name)
+    return posts
+
+@cache
+def find_tags(*tag_names):
+    posts = Post.objects(tags__name__all=tag_names)
+    return posts
+
+@cache
+def find_author(author_name):
+    author = Autor.objects(fullname__contains=author_name).first()
+    if author:
+        posts = Post.objects(author=author)
+        return posts
+    return []  
+
+
+def main():
+    while True:
+        user_input = input()
+        action = user_input.split(':')
+        match action[0]:
+            case 'tag':
+                result = find_tag(action[1])
+                [pprint(r.quote) for r in result]
+            case 'tags':
+                tags = action[1].split(',')
+                result = find_tags(*tags)
+                # print(result)
+                [pprint(r.quote) for r in result]
+            case 'name':
+                result = find_author(action[1])
+                [pprint(r.quote) for r in result]
+            case 'exit':
+                break
+            case _:
+                print("Unknown command")
+
+if __name__ == "__main__":
+    main()
---
+++ connect.py
@@ -0,0 +1,36 @@
+import configparser
+import pathlib
+
+from mongoengine import connect
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import redis
+from redis_lru import RedisLRU
+
+
+# docker run --name redis-cache -d -p 6379:6379 redis
+r = redis.Redis(host="localhost", port=6379, password=None, db=0)
+cache = RedisLRU(r)
+
+file_config = pathlib.Path(__file__).parent.joinpath('config.ini')
+config = configparser.ConfigParser()
+config.read(file_config)
+
+# username = config.get('_DB', 'USER')
+# password = config.get('_DB', 'PASSWORD')
+# database = config.get('_DB', 'DB_NAME')
+
+# uri = f"mongodb+srv://{user}:{password}@{database}/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
+# session_hw = connect("home_work_8",host=uri, ssl=True)
+
+
+# sample_con = client.sample_airbnb
+
+# if __name__ == "__main__":
+    # Send a ping to confirm a successful connection
+    # data = sample_con.listingsAndReviews.find().limit(2)
+    # for i in data:
+    # print(f'{i["name"]}:{i["description"]}')
